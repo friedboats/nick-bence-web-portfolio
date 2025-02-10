87d182ba11694a28902d6860f2e96c54614d80f5
Nick Bence <nick.bence@gmail.com>
2025-02-09 22:45:03 -0500

    Initial setup: Tailwind, TypeScript, Next.js structure

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <div className="bg-blue-500 text-white p-10">
+          <h1 className="text-4xl">Welcome to My Portfolio</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
