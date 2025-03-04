1072040c4990aafed321f423bacf26001d08a53e
Nick Bence <nick.bence@gmail.com>
2025-03-04 16:33:43 -0500

    chore(storybook): update to single parens

--- src/components/ui/Button.stories.tsx
+++ src/components/ui/Button.stories.tsx
@@ -1,13 +1,13 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import Button from "./Button";
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from './Button';
 
 const meta: Meta<typeof Button> = {
-  title: "UI/Button",
+  title: 'UI/Button',
   component: Button,
   argTypes: {
     variant: {
-      control: "select",
-      options: ["primary", "secondary", "link"],
+      control: 'select',
+      options: ['primary', 'secondary', 'link'],
     },
   },
 };
@@ -17,21 +17,21 @@ type Story = StoryObj<typeof Button>;
 
 export const Primary: Story = {
   args: {
-    children: "Click Me",
-    variant: "primary",
+    children: 'Click Me',
+    variant: 'primary',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    children: "Click Me",
-    variant: "secondary",
+    children: 'Click Me',
+    variant: 'secondary',
   },
 };
 
 export const Link: Story = {
   args: {
-    children: "Click Me",
-    variant: "link",
+    children: 'Click Me',
+    variant: 'link',
   },
 };
