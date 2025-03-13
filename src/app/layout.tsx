b175852aac1f096280990a787d7354ecc7ad90ac
Nick Bence <nick.bence@gmail.com>
2025-03-13 00:20:31 -0400

    chore: add main font family to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import '../styles/globals.css';
 
 export const metadata: Metadata = {
@@ -15,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="font-primary">{children}</body>
     </html>
   );
 }
