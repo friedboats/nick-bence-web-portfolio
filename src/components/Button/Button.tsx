da63b36456230e0400ff9e200de9089d2e73d218
Nick Bence <nick.bence@gmail.com>
2025-04-03 14:51:36 -0400

    style: add padding y to link button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -19,7 +19,7 @@ const Button: React.FC<ButtonProps> = ({
       'px-4 py-2 rounded-xl text-button-primary-default hover:text-button-primary-hover active:text-button-primary-active bg-surface-button-primary-default hover:bg-surface-button-primary-hover active:bg-surface-button-primary-active',
     secondary:
       'px-4 py-2 rounded-xl text-button-secondary-default hover:text-button-secondary-hover active:text-button-secondary-active bg-surface-button-secondary-default hover:bg-surface-button-secondary-hover active:bg-surface-button-secondary-active border-2 border-solid border-btn-border-secondary-default hover:border-btn-border-secondary-hover active:border-btn-border-secondary-active',
-    link: 'px-0 py-0 text-button-link-default hover:text-button-link-hover active:text-button-link-active hover:underline',
+    link: 'px-0 py-1 text-button-link-default hover:text-button-link-hover active:text-button-link-active hover:underline',
   };
   const disabledStyles = 'cursor-not-allowed opacity-50';
 
