6831504d91fd4d7743d4c33275dfb4986af7fd88
Nick Bence <nick.bence@gmail.com>
2025-04-08 16:28:37 -0400

    chore: clean up animatedBackground files and just use the main one

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import Navigation from '@/components/Navigation';
+import { AnimatedBackground } from '@/components/SVGComponents';
 import type { Metadata } from 'next';
 import '../styles/globals.css';
-import Navigation from '@/components/Navigation';
-import SvgAnimatedBackground from '@/components/SVGComponents/SvgAnimatedBackground';
 
 export const metadata: Metadata = {
   title: 'Nick Bence | Senior UX/UI Engineer & Front-End Developer',
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="font-primary bg-surface-background-primary">
         <div className="relative">
-          <SvgAnimatedBackground className="w-full h-screen fixed top-0 left-0" />
+          <AnimatedBackground className="w-full h-screen fixed top-0 left-0" />
           <div className="px-3 relative z-5">
             <div className="mx-auto md:max-w-[1138px] pt-4 pb-4 md:pb-15">
               <Navigation />
--- src/components/SVGComponents/AnimatedBackground.tsx
+++ src/components/SVGComponents/AnimatedBackground.tsx
@@ -1,5 +1,11 @@
-import * as React from "react";
-import type { SVGProps } from "react";
+import * as React from 'react';
+import type { SVGProps } from 'react';
+import tailwindConfig from '../../../tailwind.config';
+import resolveConfig from 'tailwindcss/resolveConfig';
+
+const fullConfig = resolveConfig(tailwindConfig);
+const primary = fullConfig.theme?.fill?.primary as string;
+const supplemental = fullConfig.theme?.fill?.supplemental as string;
 const SvgAnimatedBackground = (props: SVGProps<SVGSVGElement>) => (
   <svg
     preserveAspectRatio="xMidYMid slice"
@@ -11,40 +17,40 @@ const SvgAnimatedBackground = (props: SVGProps<SVGSVGElement>) => (
     <defs>
       <style>
         {
-          "@keyframes rotate{0%{transform:rotate(0deg)}to{transform:rotate(360deg)}}"
+          '@keyframes rotate{0%{transform:rotate(0deg)}to{transform:rotate(360deg)}}'
         }
       </style>
     </defs>
     <path
-      fill="#9b5de5"
+      fill={supplemental}
       d="M37-5C25.1-14.7 5.7-19.1-9.2-10-28.5 1.8-32.7 31.1-19.8 49c15.5 21.5 52.6 22 67.2 2.3C59.4 35 53.7 8.5 37-5"
       style={{
-        animation: "rotate 20s linear infinite",
-        transformOrigin: "13px 25px",
+        animation: 'rotate 20s linear infinite',
+        transformOrigin: '13px 25px',
       }}
     />
     <path
-      fill="#f15bb5"
+      fill={primary}
       d="M20.6 4.1C11.6 1.5-1.9 2.5-8 11.2-16.3 23.1-8.2 45.6 7.4 50S42.1 38.9 41 24.5C40.2 14.1 29.4 6.6 20.6 4.1"
       style={{
-        animation: "rotate 10s linear infinite",
-        transformOrigin: "13px 25px",
+        animation: 'rotate 10s linear infinite',
+        transformOrigin: '13px 25px',
       }}
     />
     <path
-      fill="#00bbf9"
+      fill={supplemental}
       d="M105.9 48.6c-12.4-8.2-29.3-4.8-39.4.8-23.4 12.8-37.7 51.9-19.1 74.1s63.9 15.3 76-5.6c7.6-13.3 1.8-31.1-2.3-43.8-3.5-10.8-6.4-19.8-15.2-25.5"
       style={{
-        animation: "rotate 25s linear infinite",
-        transformOrigin: "84px 93px",
+        animation: 'rotate 25s linear infinite',
+        transformOrigin: '84px 93px',
       }}
     />
     <path
-      fill="#00f5d4"
+      fill={primary}
       d="M102 67.1c-9.6-6.1-22-3.1-29.5 2-15.4 10.7-19.6 37.5-7.6 47.8s35.9 3.9 44.5-12.5c6.1-11.8 4.5-29.8-7.4-37.3"
       style={{
-        animation: "rotate 15s linear infinite",
-        transformOrigin: "84px 93px",
+        animation: 'rotate 15s linear infinite',
+        transformOrigin: '84px 93px',
       }}
     />
   </svg>
