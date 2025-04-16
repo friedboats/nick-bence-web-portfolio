3875b196ba1ac77bd0ad719bfe070ecd884812da
Nick Bence <nick.bence@gmail.com>
2025-04-16 12:04:12 -0400

    refactor: update website title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import type { Metadata } from 'next';
 import '../styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'Nick Bence | Senior UX/UI Engineer & Front-End Developer',
+  title: 'Nick Bence | Senior UI / UX Engineer',
   description:
-    'Nick Bence is a Senior UX/UI Engineer based in Detroit, MI. Specializing in front-end development and user-centered design, Nick creates intuitive, visually engaging digital experiences. Explore a portfolio of innovative work across UX design, interface development, and interactive web applications.',
+    'Nick Bence is a Senior UI / UX Engineer based in Detroit, MI. Specializing in front-end development and user-centered design, Nick creates intuitive, visually engaging digital experiences. Explore a portfolio of innovative work across UX design, interface development, and interactive web applications.',
 };
 
 export default function RootLayout({
