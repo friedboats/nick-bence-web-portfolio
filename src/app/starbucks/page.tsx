488a9d92e1fecd65e17c3cd57316f833fc9fa963
Nick Bence <nick.bence@gmail.com>
2025-04-29 00:09:17 -0400

    feat: add starbucks copy and clean up

--- src/app/starbucks/page.tsx
+++ src/app/starbucks/page.tsx
@@ -1,46 +1,11 @@
 'use client';
 
-import AssetContainer from '@/components/AssetContainer';
 import CircleImage from '@/components/CircleImage';
-import ThumbGrid from '@/components/ThumbGrid';
 
 export default function Starbucks() {
-  const thumbTestData = [
-    { src: '/test-thumb.png', alt: 'Test thumb image' },
-    { src: '/test-thumb.png', alt: 'Test thumb image' },
-    { src: '/test-thumb.png', alt: 'Test thumb image' },
-    { src: '/test-thumb.png', alt: 'Test thumb image' },
-    { src: '/test-thumb.png', alt: 'Test thumb image' },
-    { src: '/test-thumb.png', alt: 'Test thumb image' },
-    { src: '/test-thumb.png', alt: 'Test thumb image' },
-  ];
-
-  const testFunc = () => {
-    console.log('click test');
-  };
-
   return (
-    <main className="fade-in text-center w-full max-w-[1120px] mx-auto">
-      <AssetContainer
-        alt="test"
-        src="/starbucks-featured-main.png"
-        width={427}
-        height={288}
-        onClick={testFunc}
-      />
-      <div className="flex justify-center">
-        <CircleImage name="starbucks"></CircleImage>
-      </div>
-      <div className="flex justify-center my-8">
-        <AssetContainer
-          alt="test"
-          src="/starbucks-featured-main.png"
-          width={427}
-          height={288}
-          onClick={testFunc}
-        />
-      </div>
-      <ThumbGrid items={thumbTestData} />
-    </main>
+    <div className="flex justify-center">
+      <CircleImage name="starbucks"></CircleImage>
+    </div>
   );
 }
---
+++ src/copydeck/StarbucksCopy.ts
@@ -0,0 +1,8 @@
+const StarbucksCopy = {
+  intro: {
+    title: 'Stasrabucks for Life',
+    role: 'Senior Interface Developer',
+  },
+};
+
+export default StarbucksCopy;
