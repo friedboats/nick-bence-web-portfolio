cc2799286a72c4fe153c1596610bfefb269e8f47
Nick Bence <nick.bence@gmail.com>
2025-05-01 21:55:26 -0400

    chore: update copy structure

--- src/copydeck/StarbucksCopy.ts
+++ src/copydeck/StarbucksCopy.ts
@@ -1,8 +1,10 @@
 const StarbucksCopy = {
-  intro: {
-    title: 'Stasrabucks for Life',
+  projectHeader: {
+    imageID: 'starbucks',
+    title: 'Starbucks for Life',
     role: 'Senior Interface Developer',
+    description: `As a Senior Web Engineer, I contributed to developing the Starbucks for Life website, a gamified platform that boosted user engagement by integrating Starbucks' rewards program with an interactive, visually engaging experience. The project focused on creating a user-friendly interface aligned with Starbucks’ brand, offering a smooth journey for participants and exciting prizes, including free coffee for life.`,
   },
-};
+} as const;
 
 export default StarbucksCopy;
