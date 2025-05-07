0c05518d2a38f47415f8a8ed2ec22267a28dc6a6
Nick Bence <nick.bence@gmail.com>
2025-05-06 22:48:46 -0400

    feat: add blockquote to starbucks page

--- src/app/starbucks/page.tsx
+++ src/app/starbucks/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import AssetContainer from '@/components/AssetContainer';
+import BlockQuote from '@/components/BlockQuote';
 import ProjectHeader from '@/components/ProjectHeader';
+import GlobalCopy from '@/copydeck/GlobalCopy';
 import StarbucksCopy from '@/copydeck/StarbucksCopy';
 
 export default function Starbucks() {
@@ -14,6 +16,7 @@ export default function Starbucks() {
         height={760}
         src="/starbucks_app_video.mp4"
       />
+      <BlockQuote quote={GlobalCopy.quotes.whatHesAccomplished} />
     </>
   );
 }
