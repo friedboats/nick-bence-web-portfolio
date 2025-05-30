4192e114543d1c2a359cbedd54b2c28e2f046d48
Nick Bence <nick.bence@gmail.com>
2025-05-30 15:00:39 -0400

    copy: add new quotes

--- src/copydeck/GlobalCopy.ts
+++ src/copydeck/GlobalCopy.ts
@@ -13,6 +13,9 @@ const GlobalCopy = {
     goToGuys: `<p>I'd say, without question, he is one of my top "go to" guys for any questions on development. A trusted and enthusiastic source.</p>`,
     whatHesAccomplished: `<p>What he's accomplished with code to bring creative to life is second to none. He strives for the best. He'll gather and communicate between teams to find the best solutions.</br>He's truly a leader.</p>`,
     professionalism: `<p>I am impressed - haven't come across this level of professionalism recently at Dynatrace.</p>`,
+    creativeSide: `<p>Nick is a kind, level-headed, and talented developer with a passion for achieving more within these walls. He has a creative side that always shines through in his builds.</p>`,
+    teamSport: `<p>I like how Nick embodies the idea of "We are the ultimate team sport." He works wonderfully across teams, especially with the creative team and within the ID team.</p>`,
+    brainstorming: `<p>When brainstorming or concepting, Nick digs deep for alternative solutions when opportunities seem out of scope. He is an expert and is often looked to for creative solutions.</p>`,
   },
   urls: {
     figma:
