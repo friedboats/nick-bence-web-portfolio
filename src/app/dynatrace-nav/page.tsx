50f9e84a27aa6927448848e1544121dc29ebe0b7
Nick Bence <nick.bence@gmail.com>
2025-06-02 22:49:08 -0400

    feat: add dt nav work page

--- src/app/dynatrace-nav/page.tsx
+++ src/app/dynatrace-nav/page.tsx
@@ -1,9 +1,48 @@
-import CircleImage from '@/components/CircleImage';
+import BlockQuote from '@/components/BlockQuote';
+import { Carousel } from '@/components/Carousel';
+import ProjectHeader from '@/components/ProjectHeader';
+import TextSection from '@/components/TextSection';
+import DynatraceNavCopy from '@/copydeck/DynatraceNavCopy';
+import GlobalCopy from '@/copydeck/GlobalCopy';
 
 export default function DynatraceNav() {
   return (
-    <main className="fade-in flex justify-center w-full max-w-[1120px] mx-auto">
-      <CircleImage name="dynatrace"></CircleImage>
-    </main>
+    <>
+      <ProjectHeader copy={DynatraceNavCopy} />
+      <div className="mt-5">
+        <Carousel data={DynatraceNavCopy.projectHeader.carousel} />
+      </div>
+      <div className="section-spacing">
+        <TextSection
+          layout="right"
+          title={DynatraceNavCopy.challenge.title}
+          body={DynatraceNavCopy.challenge.body}
+          images={DynatraceNavCopy.challenge.assets}
+        ></TextSection>
+      </div>
+      <div className="section-spacing">
+        <TextSection
+          title={DynatraceNavCopy.competitiveAnalysis.title}
+          body={DynatraceNavCopy.competitiveAnalysis.body}
+          images={DynatraceNavCopy.competitiveAnalysis.assets}
+        ></TextSection>
+      </div>
+      <div className="section-spacing">
+        <TextSection
+          title={DynatraceNavCopy.cardSorting.title}
+          body={DynatraceNavCopy.cardSorting.body}
+          images={DynatraceNavCopy.cardSorting.assets}
+        ></TextSection>
+      </div>
+      <div className="section-spacing">
+        <TextSection
+          title={DynatraceNavCopy.impact.title}
+          body={DynatraceNavCopy.impact.body}
+        ></TextSection>
+      </div>
+      <div className="section-spacing">
+        <BlockQuote quote={GlobalCopy.quotes.professionalism} />
+      </div>
+    </>
   );
 }
---
+++ src/copydeck/DynatraceNavCopy.ts
@@ -0,0 +1,125 @@
+const DynatraceNavCopy = {
+  projectHeader: {
+    imageID: 'dynatrace',
+    title: 'Dynatrace Nav Refresh',
+    role: 'Lead UX Engineer / UI Design',
+    description: `<p>As the UX Lead at Dynatrace, I spearheaded the redesign of the platform's navigation to enhance user experience, simplify workflows, and make critical features more accessible. The primary objectives were to improve the intuitiveness of the navigation system, reduce cognitive load, and ensure that the new design aligned with both user needs and business goals.</p>`,
+    carousel: [
+      {
+        alt: 'Dynatrace navigation upgrade project',
+        src: '/dynatrace-navigation-featured-main.png',
+      },
+      {
+        alt: 'Starbucks for life project',
+        src: '/starbucks-featured-main.png',
+      },
+      {
+        alt: 'Dynatrace platform page project',
+        src: '/dynatrace-platform-featured-main.png',
+      },
+      {
+        alt: 'Nathans hot dogs project',
+        src: '/nathans-featured-main.png',
+      },
+    ],
+  },
+  challenge: {
+    title: 'The Challenge',
+    body: '<p>The existing navigation system at Dynatrace made it difficult for users to find key information, leading to frustration and an increase in support requests. The confusing information architecture hindered access to valuable resources, ultimately risking decreased user engagement and conversions.</p>',
+    assets: [
+      {
+        src: '/dynatrace-navigation-featured-main.png',
+        alt: 'Starbucks for life project',
+      },
+    ],
+  },
+  competitiveAnalysis: {
+    title: 'Competitive Analysis',
+    body: `
+    <p>To ensure our redesign aligned with industry standards and best practices, I conducted a competitive analysis by examining the navigation structures of key players in both our industry and related markets.</p>
+
+    <ul>
+      <li><strong>Industry Leaders:</strong> DataDog, Splunk, and New Relic provided insights into the functionalities and common patterns in the data monitoring and performance management space.</li>
+      <li><strong>Market Inspiration:</strong> We also looked to successful user-centric products like Asana, Nike, Figma, Wedevs, and Dribbble for ideas on clean, intuitive navigation, as well as innovative approaches to layout and design.</li>
+    </ul>
+  `,
+    assets: [
+      {
+        src: '/dynatrace-navigation-featured-thumb.png',
+        alt: 'Starbucks for life project',
+      },
+      {
+        src: '/dynatrace-navigation-featured-thumb.png',
+        alt: 'Starbucks for life project',
+      },
+      {
+        src: '/dynatrace-navigation-featured-thumb.png',
+        alt: 'Starbucks for life project',
+      },
+      {
+        src: '/dynatrace-navigation-featured-thumb.png',
+        alt: 'Starbucks for life project',
+      },
+      {
+        src: '/dynatrace-navigation-featured-thumb.png',
+        alt: 'Starbucks for life project',
+      },
+      {
+        src: '/dynatrace-navigation-featured-thumb.png',
+        alt: 'Starbucks for life project',
+      },
+      {
+        src: '/dynatrace-navigation-featured-thumb.png',
+        alt: 'Starbucks for life project',
+      },
+    ],
+  },
+  cardSorting: {
+    title: 'Closed Card Sorting',
+    body: `
+      <p>To understand how users mentally map the navigation system and to inform the structure, we conducted a closed card sorting study with 15-20 participants along with an exit survey. Using the UseBerry platform, participants were asked to categorize 40-80 items from the navigation menu.</p>
+      <div classname="ul-group">
+      <strong>Key Findings:</strong>
+      <ul>
+        <li>We identified common groupings of content and category names that made the most sense to users.</li>
+        <li>We discovered common themes and frequently paired items, giving us insight into user priorities.</li>
+        <li>The research revealed discrepancies between user assumptions (mental models) and the actual structure of the current navigation, helping us clarify and align the design with real user needs.</li>
+      </ul>
+      </div>
+    `,
+    assets: [
+      {
+        src: '/dynatrace-navigation-featured-main.png',
+        alt: 'Starbucks for life project',
+      },
+      {
+        src: '/dynatrace-navigation-featured-main.png',
+        alt: 'Starbucks for life project',
+      },
+    ],
+  },
+  efficiency: {
+    title: 'Boosting Efficiency',
+    body: '<p>Managing frequent content updates and new animations was a key challenge for the Starbucks for Life promotion. To address this, my colleague and I developed a custom HTML canvas tool that enabled quick and seamless animation updates. This innovation significantly reduced the time and effort required for content management, proving to be a major time saver and an invaluable asset for future campaigns.</p>',
+    assets: [
+      {
+        src: '/dynatrace-navigation-featured-main.png',
+        alt: 'Starbucks for life project',
+      },
+    ],
+  },
+  impact: {
+    title: 'Results, Impact & Key Takeaways',
+    body: `
+    <p>This project highlighted the importance of seamless collaboration across design, development, and project management to deliver a high-impact experience.</p>
+
+    <ul>
+      <li><strong>30% increase in user engagement:</strong> Gamified experience boosted interaction.</li>
+      <li><strong>20% increase in sign-ups:</strong> Higher conversion rate from visitors to participants.</li>
+      <li><strong>Positive feedback:</strong> Enthusiastic reviews earned the project a gold star.</li>
+    </ul>
+  `,
+  },
+} as const;
+
+export default DynatraceNavCopy;
